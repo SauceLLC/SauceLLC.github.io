fff774d6d2b107a6d4dd7a8daaf6a31372bab8b8
Justin Mayfield <tooker@gmail.com>
2020-04-06 16:05:01 -0600

    html push state nav

--- js/main.js
+++ js/main.js
@@ -3,6 +3,31 @@
         const isFirefox = navigator.userAgent.indexOf("Firefox") !== -1;
         const installLink = document.querySelector(`a.store-install.${isFirefox ? 'firefox' : 'default'}`);
         installLink.classList.remove('hidden');
+
+        const pages = ['features', 'about', 'patron', 'support'];
+        for (const x of pages) {
+            if (location.pathname.match(new RegExp(`^\\/${x}(\\.html)?(\\?|$|#)`))) {
+                const anchor = document.querySelector(`#${x}`);
+                anchor.scrollIntoView();
+                break;
+            }
+        }
+        const anchorLinks = document.querySelectorAll('a.anchor');
+        for (const x of anchorLinks) {
+            x.addEventListener('click', ev => {
+                let page = ev.currentTarget.hash.substr(1);
+                if (page === 'top') {
+                    page = '/'; 
+                }
+                ga('set', {page});
+                ga('send', 'pageview');
+                if (window.history && history.pushState) {
+                    history.pushState(null, null, page);
+                    document.querySelector(ev.currentTarget.hash).scrollIntoView();
+                    ev.preventDefault();
+                }
+            });
+        }
     }
     addEventListener('load', load);
 })();

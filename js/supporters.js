9c51caf3ab751e5ac056e6763906fd722404e372
Justin Mayfield <tooker@gmail.com>
2020-11-27 22:15:56 -0700

    Make patron supporters  list a json file

--- js/supporters.js
+++ js/supporters.js
@@ -1,4 +1,14 @@
 (function() {
+    const patrons = (async function() {
+        const uri = location.protocol === 'file:' ? 'https://saucellc.io/' : '/';
+        const [brags, supporters] = await Promise.all([
+            fetch(uri + 'brags.json').then(x => x => x.json()),
+            fetch(uri + 'supporters.json').then(x => x => x.json())
+        ]);
+        return {brags, supporters};
+    })();
+        
+
     function shuffle(data) {
         for (let i = data.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
@@ -19,22 +29,24 @@
     }
 
     async function onDOMLoaded() {
-        const brags = await (await fetch('https://saucellc.io/brags.json')).json();
-        const availBrags = Array.from(brags);
-        const patrons = document.querySelectorAll(`patron`);
         const orders = [];
-        for (let i = 0; i < patrons.length; i++) {
+        const {brags, supporters} = await patrons;
+        for (let i = 0; i < supporters.length; i++) {
             orders.push(i);
         }
-        let order = 0;
+        const frag = document.createDocumentFragment();
         for (const i of shuffle(orders)) {
+            const patron = document.createElement('patron');
+            patron.textContent = supporters[i];
             const brag = document.createElement('div');
             brag.classList.add('brag');
-            const p = patrons[i];
             brag.textContent = brags[i % brags.length];
-            p.appendChild(brag);
-            p.style.order = order++;
+            patron.appendChild(brag);
+            frag.appendChild(patron);
         }
+        document.querySelector('section.supporters').appendChild(frag);
+            
+        //window.scrollTo(0, 0);
     }
 
     addEventListener('DOMContentLoaded', onDOMLoaded);

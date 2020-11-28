cf65830fbefab5412ce6593beaa2e4cc14585435
Justin Mayfield <tooker@gmail.com>
2020-11-27 21:44:02 -0700

    Use consistent hashes with patrons.

--- js/supporters.js
+++ js/supporters.js
@@ -9,14 +9,24 @@
         return data;
     }
 
+    function hash(str) {
+        let hash = 0;
+        for (let i = 0; i < str.length; i++) {
+            hash = ((hash << 5) - hash) + str.charCodeAt(i);
+            hash |= 0;
+        }
+        return hash;
+    }
+
     async function onDOMLoaded() {
-        const brags = shuffle(await (await fetch('https://saucellc.io/brags.json')).json());
+        const brags = await (await fetch('https://saucellc.io/brags.json')).json();
+        const availBrags = Array.from(brags);
         const patrons = document.querySelectorAll(`patron`);
         let i = 0;
         for (const x of patrons) {
             const brag = document.createElement('div');
             brag.classList.add('brag');
-            brag.textContent = brags[i++ % brags.length];
+            brag.textContent = availBrags.splice(Math.abs(hash(x.textContent)) % availBrags.length - 1, 1)[0];
             x.appendChild(brag);
         }
     }

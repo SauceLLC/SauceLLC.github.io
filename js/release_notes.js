176a5cb1a77627af602ad68a2c51eeda3226db8d
Justin Mayfield <tooker@gmail.com>
2021-03-20 17:29:14 -0600

    Add release notes JSON and page.

---
+++ js/release_notes.js
@@ -0,0 +1,43 @@
+(function() {
+    const notesPromise = (async function() {
+        const uri = location.protocol === 'file:' ? 'https://saucellc.io/' : '/';
+        return await fetch(uri + 'release_notes.json').then(x => x.json());
+    })();
+
+
+    function notesToHTML(notes) {
+        const html = [];
+        for (const x of notes) {
+            if (typeof x === 'string') {
+                html.push(`<li>${x}</li>`);
+            } else {
+                html.push(`<li>${x.html}</li>`);
+                if (x.children && x.children.length) {
+                    html.push(`<ul>${notesToHTML(x.children)}</ul>`);
+                }
+            }
+        }
+        return html.join('');
+    }
+
+
+    async function onDOMLoaded() {
+        const notes = await notesPromise;
+        const frag = document.createDocumentFragment();
+        for (const x of notes.reverse()) {
+            const release = document.createElement('release');
+            release.id = x.version;
+            release.innerHTML = `
+                <header>
+                    <div class="version">v${x.version}</div>
+                    <div class="date">${new Date(x.ts).toLocaleDateString()}</div>
+                </header>
+                <ul class="notes">${notesToHTML(x.notes)}</ul>
+            `;
+            frag.appendChild(release);
+        }
+        document.querySelector('section.notes').appendChild(frag);
+    }
+
+    addEventListener('DOMContentLoaded', onDOMLoaded);
+})();
--- js/supporters.js
+++ js/supporters.js
@@ -61,4 +61,3 @@
 
     addEventListener('DOMContentLoaded', onDOMLoaded);
 })();
-

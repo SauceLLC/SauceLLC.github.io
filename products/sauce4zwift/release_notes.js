d94b6a8dd103df47e9c22ad78897db2e7dd444c8
Justin Mayfield <tooker@gmail.com>
2022-06-24 16:38:14 -0600

    Rel notes for s4z

---
+++ products/sauce4zwift/release_notes.js
@@ -0,0 +1,45 @@
+(function() {
+    const notesPromise = (async function() {
+        return await fetch('release_notes.json').then(x => x.json());
+    })();
+
+
+    function notesToHTML(notes) {
+        const html = [];
+        for (const x of notes) {
+            if (typeof x === 'string') {
+                html.push(`<li>${x}</li>`);
+            } else {
+                html.push(`<li>${x.html}</li>`);
+                if (x.children && x.children.length) {
+                    html.push(`<ul>${notesToHTML(x.children)}</ul>`);
+                }
+            }
+        }
+        return html.join('');
+    }
+
+
+    async function onDOMLoaded() {
+        const notes = await notesPromise;
+        const frag = document.createDocumentFragment();
+        for (const x of notes.reverse()) {
+            if (x.private) {
+                continue;
+            }
+            const release = document.createElement('release');
+            release.id = x.version;
+            release.innerHTML = `
+                <header>
+                    <div class="version">v${x.version}</div>
+                    <div class="date">${new Date(x.ts).toLocaleDateString()}</div>
+                </header>
+                <ul class="notes">${notesToHTML(x.notes)}</ul>
+            `;
+            frag.appendChild(release);
+        }
+        document.querySelector('section.notes').appendChild(frag);
+    }
+
+    addEventListener('DOMContentLoaded', onDOMLoaded);
+})();
